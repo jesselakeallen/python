ce6f8ed871bae3261f20ab9f48551bdbc1f27d92
Jesse Allen <jesse.lake.allen@gmail.com>
2018-10-16 13:15:28 -0500

    finish unit 3

---
+++ web_crawler.py
@@ -0,0 +1,36 @@
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def get_all_links(page):
+    links = []
+    while True:
+	url, endpos = get_next_target(page)
+	if url:
+		links.append(url)
+		page = page[endpos:]
+	else:
+		break
+    return links
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+
+
+# start with tocrawl = [seed]
+# crawled = []
+# while there are more pages tocrawl:
+#     pick a page from tocrawl
+#     add that page to crawled
+#     add all the link targets on this page to tocrawl
+# return crawled
